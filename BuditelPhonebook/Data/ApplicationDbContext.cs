537a27747bb18f342d70f62e82a25681b70eef07
lechev95 <lecho.lechev@gmail.com>
2024-12-19 14:18:47 +0200

    DB context updated

--- BuditelPhonebook/Data/ApplicationDbContext.cs
+++ BuditelPhonebook/Data/ApplicationDbContext.cs
@@ -5,13 +5,13 @@ namespace BuditelPhonebook.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Person> People { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
     }
 
 }
